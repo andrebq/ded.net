a632d7755db6c9998a76baf67cbef803b0f78964
andrebq <andrebq@gmail.com>
2020-11-29 22:15:26 +0100

    chore: not working...

---
+++ Ded.Net/Ded.Net.Editor/TextGridView.cs
@@ -0,0 +1,20 @@
+﻿using Avalonia.Media;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ded.Net.Editor
+{
+    /// <summary>
+    /// TextGridView is composed by a grid of TextCell
+    /// </summary>
+    public class TextGridView : Avalonia.Controls.Control
+    {
+        public override void Render(DrawingContext context)
+        {
+            base.Render(context);
+            var b = Brush.Parse("red");
+            context.FillRectangle(b, this.Bounds, 0.0f);
+        }
+    }
+}
