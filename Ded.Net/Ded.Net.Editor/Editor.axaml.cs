88200da164ac5bea78c77234b5c96d89bf74db45
andrebq <andrebq@gmail.com>
2020-11-29 22:05:13 +0100

    chore: trying to render the button bar + some text

---
+++ Ded.Net/Ded.Net.Editor/Editor.axaml.cs
@@ -0,0 +1,19 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Ded.Net.Editor
+{
+    public class Editor : UserControl
+    {
+        public Editor()
+        {
+            this.InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
